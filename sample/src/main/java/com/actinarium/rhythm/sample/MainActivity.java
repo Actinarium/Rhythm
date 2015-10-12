b12f12784b07a1452a7c510fbc7ac70b2bc5bc05
Actine <actinate@gmail.com>
2015-10-13 01:44:18 +0300

    Applied copyright, committed one overlooked launcher icon, removed redundant application test, removed redundant menu

--- sample/src/main/java/com/actinarium/rhythm/sample/MainActivity.java
+++ sample/src/main/java/com/actinarium/rhythm/sample/MainActivity.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 Actinarium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.actinarium.rhythm.sample;
 
 import android.app.ActivityManager;
@@ -8,8 +24,6 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
 import com.actinarium.rhythm.RhythmControl;
@@ -44,27 +58,5 @@ public class MainActivity extends AppCompatActivity {
 //        subView.getOverlay().add(drawable);
 //        drawable.setBackgroundDrawable(subView.getBackground());
         subView.setForeground(drawable);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
--- sample/src/main/java/com/actinarium/rhythm/sample/RhythmSampleApplication.java
+++ sample/src/main/java/com/actinarium/rhythm/sample/RhythmSampleApplication.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015 Actinarium
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.actinarium.rhythm.sample;
 
 import android.app.Application;
@@ -36,7 +52,9 @@ public class RhythmSampleApplication extends Application implements RhythmContro
                                 .addLayer(new Guide(Gravity.LEFT, (int) (24 * density)))
                                 .addLayer(new Guide(Gravity.RIGHT, (int) (24 * density)))
                                 .addLayer(new Guide(Gravity.LEFT, (int) (24 * density)).thickness((int) (8 * density)).color(Guide.DEFAULT_HIGHLIGHT_COLOR).alignOutside(false))
-                                .addLayer(new Guide(Gravity.RIGHT, (int) (24 * density)).thickness((int) (8 * density)).color(Guide.DEFAULT_HIGHLIGHT_COLOR).alignOutside(false))
+                                .addLayer(new Guide(Gravity.RIGHT, (int) (24 * density)).thickness((int) (8 * density))
+                                        .color(Guide.DEFAULT_HIGHLIGHT_COLOR)
+                                        .alignOutside(false))
                                 .addLayer(new Guide(Gravity.TOP, (int) (24 * density)))
                                 .addLayer(new Guide(Gravity.BOTTOM, (int) (24 * density)))
                                 .addLayer(new Guide(Gravity.TOP, (int) (24 * density)).thickness((int) (8 * density)).color(Guide.DEFAULT_HIGHLIGHT_COLOR).alignOutside(false))
